e4851f2abcfbb06c118032a475a8e5bbb6368e49
mannat0711-tech <tharejateshika@gmail.com>
2025-11-02 13:59:28 +0530

    Update ques 15.c

--- ques 15.c
+++ ques 15.c
@@ -5,37 +5,28 @@ Output: arr[] = {5, 1, 2, 3, 4}
 Input: arr[] = {2, 3, 4, 5, 1}
 Output: {1, 2, 3, 4, 5}*/
 #include <stdio.h>
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = 5;
+    int i, last;
 
-(int arr[], int n) {
-    int last = arr[n - 1];  
-
-
-    for (int i = n - 1; i > 0; i--) {
-        arr[i] = arr[i - 1];
-    }
-
-    arr[0] = last; 
-}
-
-int main() {
-    int n;
-
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n);
-
-    int arr[n];
-    printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    rotateClockwise(arr, n);
-
-    printf("Array after one clockwise rotation:\n");
-    for (int i = 0; i < n; i++) {
+    printf("Original array:\n");
+    for (i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
-    printf("\n");
-
+    last = arr[n - 1];
+    for (i = n - 1; i > 0; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = last;
+    printf("\nArray after one clockwise rotation:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
+
